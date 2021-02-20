eb6733b3ef9e78749fe863f6d828cfed64129bf5
Wilderness1265 <66174169+Wilderness1265@users.noreply.github.com>
2021-02-19 20:03:59 -0500

    Delete Stronger directory

--- Stronger/src/UI/UIBasics.cpp
+++
@@ -1,8 +0,0 @@
-#include "UIBasics.h"
-
-UIBasics::UIBasics() : UIBasics::UIBasics(sf::Vector2f(0, 0), sf::Color::White) {};
-
-UIBasics::UIBasics(sf::Vector2f pos, sf::Color color) {
-    this->pos = pos;
-    this->color = color;
-}
